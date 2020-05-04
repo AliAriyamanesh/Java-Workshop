8470085837f2663c23996bc8243919c4529ffe3e
AliAriyamanesh <Ariyakhodesh@gmail.com>
2020-05-05 00:40:57 +0430

    98/11/23

---
+++ Workshop_HW1/src/com/company/Main.java
@@ -0,0 +1,13 @@
+package com.company;
+
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        char[] str = input.next().toCharArray();
+        System.out.println(str.length);
+	// write your code here
+    }
+}
